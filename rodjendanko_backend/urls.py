606621c5d0c663ad7f2cbd4e5c43d62118599ab3
Vlada <vlada.gasevic@icloud.com>
2025-04-05 23:33:56 +0200

    feat: enable user URLs and add commented login view in URL configuration

--- rodjendanko_backend/urls.py
+++ rodjendanko_backend/urls.py
@@ -21,6 +21,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt import views as jwt_views
+from django.contrib.auth import views as auth_views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -39,9 +40,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/', include('api.urls')),
-    #path('', include('users.urls')),
+    path('', include('users.urls')),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    #path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
