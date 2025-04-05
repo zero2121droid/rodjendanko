935cb60f98b1e16693896d36c44909d75de9d0c5
Vlada <vlada.gasevic@icloud.com>
2025-04-05 23:27:06 +0200

    feat: add login and logout views with corresponding template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.views import View
+from django.urls import reverse_lazy
 
-# Create your views here.
+class LoginInterfaceView(LoginView):
+    template_name = 'users/login.html'
+
+class LogoutInterfaceView(View):
+    def get(self, request):
+        logout(request)  # Logs out the user
+        return redirect('/')  # Redirect to login page
