ec008b2e6fcd07e21a82d46561740d88e80616a4
Vlada <vlada.gasevic@icloud.com>
2025-04-05 22:33:15 +0200

    fix: update STATIC_URL and STATICFILES_DIRS for correct static file handling

--- rodjendanko_backend/settings.py
+++ rodjendanko_backend/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
+    BASE_DIR / 'static',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
