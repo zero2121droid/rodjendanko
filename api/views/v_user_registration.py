741c0558b58094b35cda93f8870cad6396b711cf
Vlada <vlada.gasevic@icloud.com>
2025-04-23 15:49:52 +0200

    feat: add AllowAny permission to UserRegistrationView for open access

--- api/views/v_user_registration.py
+++ api/views/v_user_registration.py
@@ -2,8 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from api.serializers.s_user_registration import UserRegistrationSerializer
+from rest_framework.permissions import AllowAny
 
 class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
