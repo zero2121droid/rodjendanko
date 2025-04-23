778c75668dffad82a07d462fa0047fac50e8ef96
Vlada <vlada.gasevic@icloud.com>
2025-04-23 15:55:36 +0200

    feat: initialize user coins to 100 upon registration

--- api/serializers/s_user_registration.py
+++ api/serializers/s_user_registration.py
@@ -24,5 +24,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         password = validated_data.pop('password')
         user = User(**validated_data)
         user.set_password(password)
+        user.coins = 100  # postavi početni broj novčića
         user.save()
         return user
