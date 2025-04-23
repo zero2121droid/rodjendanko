82a1972761f5cc988ea1e1ebe6e4f5f91bfb558d
Vlada <vlada.gasevic@icloud.com>
2025-04-23 09:52:27 +0200

    refactor: remove obsolete Django 5.1.1 files and update to Django 5.2

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = None
     email = models.EmailField(unique=True)
     address = models.TextField(null=True, blank=True)
     city = models.CharField(max_length=255, null=True, blank=True)
