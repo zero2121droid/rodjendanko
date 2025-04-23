68a56f8442ff65e60fd708292a0ae91b0b361057
Vlada <vlada.gasevic@icloud.com>
2025-04-23 09:08:15 +0200

    refactor: comment out UUID primary key in User model

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = None
     email = models.EmailField(unique=True)
     address = models.TextField(null=True, blank=True)
