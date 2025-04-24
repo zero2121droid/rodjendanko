8ab3b213c21417441e30d837c23653c6902c607c
Vlada <vlada.gasevic@icloud.com>
2025-04-24 16:33:16 +0200

    feat: add bookings relationship to UserSerializer and enhance BookingsViewSet with user-specific queryset and permissions

--- api/serializers/s_user.py
+++ api/serializers/s_user.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from users.models import User, Children
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from api.serializers.s_bookings import BookingsSerializer
 
 # ---------------------------------------------------------------------
 # Children Serializer
@@ -16,6 +17,7 @@ class ChildrenSerializer(serializers.ModelSerializer):
 # ---------------------------------------------------------------------
 class UserSerializer(serializers.ModelSerializer):
     children = ChildrenSerializer(many=True, read_only=True, source='children_set')
+    bookings = BookingsSerializer(many=True, read_only=True, source='bookings_set')
     password = serializers.CharField(write_only=True)
 
     class Meta:
--- api/views/v_bookings.py
+++ api/views/v_bookings.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from reservations.models import Bookings
 from api.serializers.s_bookings import BookingsSerializer
@@ -9,12 +9,19 @@ from notifications.utils import create_notification
 class BookingsViewSet(viewsets.ModelViewSet):
     queryset = Bookings.objects.all()
     serializer_class = BookingsSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ["customer", "location", "service", "booking_date"]
-    filterset_fields = ["customer"]  # primer za precizno filtriranje
+    filterset_fields = ["customer", "location", "booking_date", "status"]  # primer za precizno filtriranje
     ordering_fields = ["created_at", "updated_at"]
     ordering = ["created_at"]  # defaultno sortiranje po created_at
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Bookings.objects.all()
+        return Bookings.objects.filter(user=user)
 
     def perform_create(self, serializer):
         booking = serializer.save(user=self.request.user)
