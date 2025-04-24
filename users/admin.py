890aa5f023ab89ff42d81fc5fdf757000f37ac4c
Vlada <vlada.gasevic@icloud.com>
2025-04-24 11:15:57 +0200

    fix: update get_coins method to handle missing coins_wallet and return coins_balance

--- users/admin.py
+++ users/admin.py
@@ -4,9 +4,10 @@ from .models import User, Children
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     def get_coins(self, obj):
-        return obj.coins_wallet.balance if obj.coins_wallet else 0
+        wallet = getattr(obj, 'coins_wallet', None)
+        return wallet.coins_balance if wallet else 0
     get_coins.short_description = 'Coins'
-    
+
     list_display = ('id', 'first_name', 'last_name','email', 'phone', 'get_coins', 'created_at')
     search_fields = ('username', 'firstname', 'lastname','email')
     ordering = ('-created_at',)
