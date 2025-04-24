c8a7727eeb97a0800594b0134e2804a56b80e879
Vlada <vlada.gasevic@icloud.com>
2025-04-24 10:54:12 +0200

    fix: remove password2 field from UserRegistrationSerializer and update create method to handle password hashing

--- api/serializers/s_user_registration.py
+++ api/serializers/s_user_registration.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from users.models import User
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, style={"input_type": "password"})
     password2 = serializers.CharField(write_only=True, style={"input_type": "password"})  # za potvrdu lozinke
 
     class Meta:
         model = User
         fields = ['username','email', 'password', 'password2', 'address','city', 'phone', 'description']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
@@ -19,4 +21,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"password": "Lozinke se ne poklapaju."})
         return attrs
 
-    
+    def create(self, validated_data):
+        validated_data.pop('password2')  # Uklanjamo potvrdu lozinke
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Hash-uj lozinku
+        user.save()
+        return user
