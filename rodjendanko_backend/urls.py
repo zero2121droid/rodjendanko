6c7bc990069393e90e0456ab8bfa1064fccac91d
Vlada <vlada.gasevic@icloud.com>
2025-04-28 13:42:06 +0200

    refactor: remove permission and authentication classes from schema view for improved security

--- rodjendanko_backend/urls.py
+++ rodjendanko_backend/urls.py
@@ -36,7 +36,8 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=[],
+    authentication_classes=[],
 )
 
 urlpatterns = [
