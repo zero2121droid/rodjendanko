7095ec8ebafd6c95ea44eeee5e657e69615635f1
Vlada <vlada.gasevic@icloud.com>
2025-04-28 14:12:26 +0200

    refactor: add Swagger tags decorator to CustomerRegistrationView for improved API documentation

--- api/views/v_customer_registration.py
+++ api/views/v_customer_registration.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from api.serializers.s_customer_registration import CustomerRegistrationSerializer
 from rest_framework.permissions import AllowAny
+from api.utils import add_swagger_tags
 
+@add_swagger_tags(tags=["Bookings"])
 class CustomerRegistrationView(APIView):
     permission_classes = [AllowAny]
 
