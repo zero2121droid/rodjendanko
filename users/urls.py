e784e575eee37de4614c6dc1ebe48fb47a95f937
Vlada <vlada.gasevic@icloud.com>
2025-04-28 11:49:23 +0200

    refactor: update user URL patterns to include LoginInterfaceView and improve routing structure

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 from .views import LogoutView
 
 urlpatterns = [
+    path('', views.LoginInterfaceView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
