eea9bf6a300c056b99f1d58662ce767a5e87b1c0
Vlada <vlada.gasevic@icloud.com>
2025-04-28 13:33:31 +0200

    refactor: remove commented-out LoginInterfaceView path from user URL patterns

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 from .views import LogoutView
 
 urlpatterns = [
-    path('', views.LoginInterfaceView.as_view(), name='login'),
+    #path('', views.LoginInterfaceView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
