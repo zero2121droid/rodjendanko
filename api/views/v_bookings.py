ca0c267da0ef9b2544bded4b0ddf6b9124c23780
Vlada <vlada.gasevic@icloud.com>
2025-04-28 14:05:01 +0200

    refactor: add Swagger tags decorator to BookingsViewSet for improved API documentation

--- api/views/v_bookings.py
+++ api/views/v_bookings.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from reservations.models import Bookings
 from api.serializers.s_bookings import BookingsSerializer
 from notifications.utils import create_notification
+from api.utils import add_swagger_tags
 
 # ---------------------------------------------------------------------
 # Bookings Permissions
@@ -28,6 +29,7 @@ class IsBookingOwnerOrCustomerOrAdmin(permissions.BasePermission):
 # ---------------------------------------------------------------------
 # Bookings ViewSet
 # ---------------------------------------------------------------------
+@add_swagger_tags(tags=["Bookings"])
 class BookingsViewSet(viewsets.ModelViewSet):
     queryset = Bookings.objects.all()
     serializer_class = BookingsSerializer
