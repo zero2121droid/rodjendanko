723b1447a83f52877c2e267488aa07eed9e7c914
Vlada <vlada.gasevic@icloud.com>
2025-05-05 17:13:43 +0200

    feat: update UserRegistrationSerializer to include owner fields and handle password hashing for partners

--- api/serializers/s_user_registration.py
+++ api/serializers/s_user_registration.py
@@ -2,15 +2,20 @@ from datetime import timezone
 from rest_framework import serializers
 from users.models import User
 from playrooms.models import Customer
+from django.contrib.auth.hashers import make_password
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, style={"input_type": "password"})  # za potvrdu lozinke
     terms_accepted = serializers.BooleanField(write_only=True)
-    
+    owner_name = serializers.CharField(write_only=True)  # Ovo polje više nije potrebno, jer koristimo username
+    owner_email = serializers.EmailField(write_only=True)  # Ovo polje više nije potrebno, jer koristimo email
+    owner_password = serializers.CharField(write_only=True)  # Ovo polje više nije potrebno, jer koristimo password
 
     class Meta:
         model = User
-        fields = ['username','email', 'password', 'password2', 'address', 'city', 'phone', 'description', 'terms_accepted', 'user_type', 'company_name', 'owner']
+        fields = ['username', 'email', 'password', 'password2', 'address', 'city', 
+                  'phone', 'description', 'terms_accepted', 'user_type', 'company_name', 'owner', 
+                  'owner_name', 'owner_email', 'owner_password']
         extra_kwargs = {
             'password': {'write_only': True},
             'is_active': {'read_only': True},
@@ -34,8 +39,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         validated_data.pop('password2')  # Uklanjamo potvrdu lozinke
         terms_accepted = validated_data.pop('terms_accepted')
         password = validated_data.pop('password')
-        user_type = serializers.ChoiceField(choices=User.USER_TYPES)
-        company_name = serializers.CharField(required=False, allow_blank=True)
+        user_type = validated_data.get('user_type')
 
         request = self.context.get('request')  # Dohvati request iz konteksta
         ip_address = None
@@ -50,7 +54,20 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         user.terms_accepted_at = timezone.now()
         user.terms_accepted_ip = ip_address
         user.save()
-        
-        if user_type == "partner" and company_name:
-            Customer.objects.create(user=user, name=company_name)
+
+        if user_type == 'partner':
+            company_name = validated_data.get('company_name')
+
+            # Kreiranje Customer objekta za partnera
+            Customer.objects.create(
+                user=user,
+                name=company_name,  # Kompanija je kreirana sa imenom iz `company_name`
+                address=validated_data['address'],
+                city=validated_data['city'],
+                phone=validated_data['phone'],
+                description=validated_data.get('description', ''),
+                owner_name=user.username,  # Vlasnik koristi username kao owner_name
+                owner_email=user.email,    # Vlasnik koristi email kao owner_email
+                owner_password=make_password(password),  # Lozinka vlasnika hešovana
+            )
         return user
