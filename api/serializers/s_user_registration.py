96e483087cd6420add0c9b855e48f084a7438f1a
Vlada <vlada.gasevic@icloud.com>
2025-05-05 17:38:42 +0200

    feat: update UserRegistrationSerializer to include owner field assignment during user creation

--- api/serializers/s_user_registration.py
+++ api/serializers/s_user_registration.py
@@ -48,6 +48,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         user = User(**validated_data)
         user.set_password(password)
         user.user_type = validated_data.get('user_type', 'user')
+        user.owner = validated_data.get('owner', False)
         user.terms_accepted = terms_accepted
         user.terms_accepted_at = timezone.now()
         user.terms_accepted_ip = ip_address
