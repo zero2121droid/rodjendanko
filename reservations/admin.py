5f0dca65eed58f8120e31b47ddc43217adabb900
Vlada <vlada.gasevic@icloud.com>
2025-05-13 22:58:49 +0200

    feat: add BookingsAdmin to enhance admin interface for bookings management

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Bookings
 
-# Register your models here.
+class BookingsAdmin(admin.ModelAdmin):
+    list_display = ('public_id', 'customer', 'user', 'status', 'booking_date', 'created_at', 'updated_at')
+    list_filter = ('status', 'user', 'customer')
+    search_fields = ('public_id', 'customer__name', 'user__email')
+    list_editable = ('status',)
+    ordering = ('-created_at',)
+    readonly_fields = ('public_id', 'created_at', 'updated_at')  
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Bookings, BookingsAdmin)
+
