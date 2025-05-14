069780df128ff1cf92315ee5443af192802476b0
Vlada <vlada.gasevic@icloud.com>
2025-05-14 21:29:54 +0200

    fix: update BookingFilter to use the rest_framework import for django_filters

--- reservations/filters.py
+++ reservations/filters.py
@@ -1,9 +1,8 @@
-import django_filters
+from django_filters import rest_framework as filters
 from reservations.models import Bookings, BookingStatus
 
-class BookingFilter(django_filters.FilterSet):
-    status = django_filters.ChoiceFilter(choices=BookingStatus.choices)
-    booking_date = django_filters.DateFilter(field_name='booking_date', lookup_expr='date')
+class BookingFilter(filters.FilterSet):
+    status = filters.ChoiceFilter(choices=BookingStatus.choices)
 
     class Meta:
         model = Bookings
