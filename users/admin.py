7fad7b64621b01534ca5563cb4b33245a78f0b33
Vlada <vlada.gasevic@icloud.com>
2025-05-16 11:22:53 +0200

    feat: add public_id to list_display and search_fields in UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -8,8 +8,8 @@ class UserAdmin(admin.ModelAdmin):
         return wallet.coins_balance if wallet else 0
     get_coins.short_description = 'Coins'
 
-    list_display = ('id', 'first_name', 'last_name','email', 'phone', 'get_coins', 'created_at')
-    search_fields = ('username', 'firstname', 'lastname','email')
+    list_display = ('id', 'public_id', 'first_name', 'last_name','email', 'phone', 'get_coins', 'created_at')
+    search_fields = ('username', 'firstname', 'lastname','email', 'public_id')
     ordering = ('-created_at',)
 
 @admin.register(Children)
