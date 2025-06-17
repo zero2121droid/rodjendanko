dd0eff10b8ace0ab921d485d6ae7e30e846f46b8
Vlada <vlada.gasevic@icloud.com>
2025-06-17 22:37:50 +0200

    feat: refactor UserAdmin to extend BaseUserAdmin and update fieldsets for better organization

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 from .models import User, Children
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     def get_coins(self, obj):
         wallet = getattr(obj, 'coins_wallet', None)
         return wallet.coins_balance if wallet else 0
     get_coins.short_description = 'Coins'
 
     list_display = ('id', 'public_id', 'first_name', 'last_name','email', 'phone', 'get_coins', 'created_at')
-    search_fields = ('username', 'firstname', 'lastname','email', 'public_id')
+    search_fields = ('username', 'first_name', 'last_name','email', 'public_id')
     ordering = ('-created_at',)
+    
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "phone")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "created_at")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+
+    # Ako koristiš `USERNAME_FIELD = 'email'`, ovo treba:
+    filter_horizontal = ("groups", "user_permissions")
 
 @admin.register(Children)
 class ChildrenAdmin(admin.ModelAdmin):
