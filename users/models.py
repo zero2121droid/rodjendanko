dd91b15665309dac95d1dba09a3d076bf3618135
Vlada <vlada.gasevic@icloud.com>
2025-07-02 19:18:40 +0200

    feat: add 'customer' user type to User model for enhanced user categorization

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     public_id = models.CharField(max_length=20, blank=True, null=True, unique=True)
     USER_TYPES = [
         ('admin', 'Admin'),
-        #('customer', 'Customer'),
+        ('customer', 'Customer'),
         ('partner', 'Partner'),
         ('user', 'Regular User'),
     ]
