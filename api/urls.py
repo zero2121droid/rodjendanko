b4ae1b2c4ba3fe0b54e9d1df8cce6dd8dba0fffe
Vlada <vlada.gasevic@icloud.com>
2025-09-14 18:53:10 +0200

    feat: register LocationCityViewSet in the API router

--- api/urls.py
+++ api/urls.py
@@ -12,6 +12,7 @@ from api.views.v_wallet import WalletViewSet, WalletTransactionViewSet
 from api.views.v_customer import CustomerViewSet
 from api.views.v_customer_registration import CustomerRegistrationView
 from api.views.v_rating import RatingViewSet
+from api.views.v_location import LocationCityViewSet
 
 
 router = DefaultRouter()
@@ -30,6 +31,7 @@ router.register(r'wallet', WalletViewSet, basename='wallet')
 router.register(r'wallet-transactions', WalletTransactionViewSet, basename='wallet-transactions')
 router.register(r'notifications', NotificationViewSet, basename='notification')
 router.register(r'ratings', RatingViewSet, basename='ratings')
+router.register(r'location-cities', LocationCityViewSet, basename='location-cities')
 
 urlpatterns = router.urls + [
     path('customer/register/', CustomerRegistrationView.as_view(), name='customer-register'),
